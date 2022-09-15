0f5e701285def0a88ff96bd9d586a57f8b852e7e
Olga Wagner <rosamarena24@gmail.com>
2022-09-15 22:57:31 +0700

    src added

--- src/main/java/SQRService.java
+++ src/main/java/SQRService.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class SQRService {
+public class SQRService {
+
+    public int CalcSqr(int lowerLimit; int upperLimit) {
+
+        int lowerLimit = 200;
+        int upperLimit = 300;
+
+        for (int i = 10; i; i++) {
+            if (i * i == 99) {
+                break;
+            }
+        }
+        return;
+
 }
